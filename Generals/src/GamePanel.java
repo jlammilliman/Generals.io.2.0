b6cd2d95ee9326396d4c7722ba9fac73700b4457
Justin Milliman <jlmillim@mtu.edu>
2021-02-12 09:40:47 -0500

    smarter tiles, cleaned variables, basic map generation, improved tile class

--- Generals/src/GamePanel.java
+++ Generals/src/GamePanel.java
@@ -5,7 +5,6 @@ import javax.swing.*;
 import gfx.ImageUtils;
 
 import java.util.Random;
-import java.util.TimerTask;
 
 public class GamePanel extends JPanel implements ActionListener{
 
@@ -57,6 +56,14 @@ public class GamePanel extends JPanel implements ActionListener{
 	Image mountain;
 	Image swamp;
 	Image windmill;
+	
+	// Tile Utilities and Values
+	static final int defaultTile = 0;
+	static final int mountainTile= 1;
+	static final int cityTile 	 = 2;
+	static final int swampTile 	 = 3;
+	static final int capitolTile = 6;
+	
 
 
 	// Tile Spread statistics    TODO: Player should be able to adjust these during game creation menu
@@ -67,11 +74,11 @@ public class GamePanel extends JPanel implements ActionListener{
 	int totalVillages	= 5;
 
 	// Controller variables		TODO This should be handled by whatever handles server
-	private int playerFocusX = 1; // Default player should be moved to capitol
-	private int playerFocusY = 1;
-	private int playerControlledTroopCount = 0; // Does not control any troops by default
-	private String playerID = "player";
-	private Color playerColor = BLUE;
+	private int pX = 1; // Default player should be moved to capitol
+	private int pY = 1;
+	private int pTroops = 0; // Does not control any troops by default
+	private String pID = "player";
+	private Color pColor = BLUE;
 	char direction = ' ';  // set to ' ' for default
 
 
@@ -115,20 +122,16 @@ public class GamePanel extends JPanel implements ActionListener{
 
 					// TODO Cellular automata
 					int type = random.nextInt((int)(5));
-					if (type == 1) {
-						map[i][j].setTileType(1); 				// 1 = Mountain
-						map[i][j].setBackgroundColor(DARKGRAY);
-						map[i][j].setIcon(mountain);
+					if (type == mountainTile) {
+						map[i][j].updateTile(mountainTile, null, DARKGRAY, 0, mountain);
+					} else if ( type == cityTile) {
+						map[i][j].updateTile(cityTile, null, DARKGRAY, 10, city);
 					}
 				}
 			}
 		}
-		map[1][1].setOwnedBy(playerID);
-		map[1][1].setBackgroundColor(playerColor);
-		map[1][1].setTroopCount(1);
-		map[1][1].setTileType(6);
-		map[1][1].setIcon(capitol);
-
+		
+		map[1][1].updateTile(capitolTile, pID, pColor, 1, capitol);
 		System.out.println("Map Generated");
 		return map;
 	}
@@ -208,13 +211,13 @@ public class GamePanel extends JPanel implements ActionListener{
 				}
 			}
 
-			if(playerFocusX > -1 && playerFocusY > -1 ) { // -1 is the null case
+			if(pX > -1 && pY > -1 ) { // -1 is the null case
 
 				//Draw a cute little Black box around the area you are looking at
 				g.setColor(BLACK);
 				g.drawRect(
-						map[playerFocusX][playerFocusY].getX() * TILESIZE - 1, 
-						map[playerFocusX][playerFocusY].getY() * TILESIZE - 1, 
+						map[pX][pY].getX() * TILESIZE - 1, 
+						map[pX][pY].getY() * TILESIZE - 1, 
 						TILESIZE + 2, 
 						TILESIZE + 2
 						);
@@ -241,9 +244,9 @@ public class GamePanel extends JPanel implements ActionListener{
 				Tile t = map[i][j]; // Grab the current tile
 				
 				// If the tile has an owner and some troops on it, increment
-				if (map[i][j].getOwnedBy() != null && map[i][j].getTroopCount() > 0) {
+				if (t.getOwnedBy() != null && t.getTroopCount() > 0) {
 					// If its a city, add a troop every time update is called
-					if(map[i][j].getTileType() == 2 || map[i][j].getTileType() == 3) {
+					if(t.getTileType() == capitolTile || t.getTileType() == cityTile) {
 						t.setTroopCount(t.getTroopCount() + 1);
 					}
 					// If its a natural tile with troops on it, add a troop every tickDelay
@@ -261,21 +264,21 @@ public class GamePanel extends JPanel implements ActionListener{
 	 *  Make sure the player cannot escape the map, or enter any obstacles
 	 */
 	public void checkCollisions() {
-		if(playerFocusX != -10 && playerFocusY != -10) { //Only do this if the null condition is not true
-			if (playerFocusX < 0 ) { 
-				playerFocusX = 0;
+		if(pX != -10 && pY != -10) { //Only do this if the null condition is not true
+			if (pX < 0 ) { 
+				pX = 0;
 			}
 			//check right border
-			if (playerFocusX >= MAP_WIDTH) { 
-				playerFocusX = MAP_WIDTH - 1;
+			if (pX >= MAP_WIDTH) { 
+				pX = MAP_WIDTH - 1;
 			}
 			//check upper border
-			if (playerFocusY < 0) {
-				playerFocusY = 0;
+			if (pY < 0) {
+				pY = 0;
 			}
 			//check lower border
-			if (playerFocusY >= MAP_HEIGHT) { 
-				playerFocusY = MAP_HEIGHT - 1;
+			if (pY >= MAP_HEIGHT) { 
+				pY = MAP_HEIGHT - 1;
 			}
 		}
 
@@ -319,40 +322,38 @@ public class GamePanel extends JPanel implements ActionListener{
 		// Increment to new Tile now that we have the previous tile all taken care of
 		// If the player is trying to move into a tile that has a mountain or other obstacle, don't let it
 		boolean checkTile = false;
-		if (map[playerFocusX + x][playerFocusY + y].getTileType() != 1) {
-			if (playerFocusX + x != 0 || playerFocusX + x != MAP_WIDTH)  { playerFocusX += x; checkTile = true; }
-			if (playerFocusY + y != 0 || playerFocusY + y != MAP_HEIGHT) { playerFocusY += y; checkTile = true; }
+		if (map[pX + x][pY + y].getTileType() != mountainTile) {
+			if (pX + x != 0 || pX + x != MAP_WIDTH)  { pX += x; checkTile = true; }
+			if (pY + y != 0 || pY + y != MAP_HEIGHT) { pY += y; checkTile = true; }
 		}
 
 
 		if (checkTile) {
 			// Make sure we collect all the troops but one before we move, only if we own them tho
-			if (map[playerFocusX - x][playerFocusY - y].getOwnedBy().compareTo(playerID) == 0 ){
-				playerControlledTroopCount = map[playerFocusX - x][playerFocusY - y].getTroopCount() - 1;
+			if (map[pX - x][pY - y].getOwnedBy().compareTo(pID) == 0 ){
+				pTroops = map[pX - x][pY - y].getTroopCount() - 1;
 
 				// If the playerFocus is moved to a new tile, check previous tile for ownership
-				if (map[playerFocusX - x][playerFocusY - y].getOwnedBy().compareTo(playerID) == 0) {
-					if (playerControlledTroopCount >= 1) { 						// Only Move troops if the player actually has any
-						map[playerFocusX - x][playerFocusY - y].setTroopCount(1); 		// Leave one guy to defend, he's got this
+				if (map[pX - x][pY - y].getOwnedBy().compareTo(pID) == 0) {
+					if (pTroops >= 1) { 						// Only Move troops if the player actually has any
+						map[pX - x][pY - y].setTroopCount(1); 	// Leave one guy to defend, he's got this
 					}
 				}
 			}
 			// Only do this is we actually have enough troops
-			if(playerControlledTroopCount >= 1) {
+			if(pTroops >= 1) {
 				// Now to check the new Tile, first we check to see if we own it or not
 				// If we do own the tile, move our troops into it
-				if (map[playerFocusX][playerFocusY].getOwnedBy().compareTo(playerID) == 0) {
-					map[playerFocusX][playerFocusY].setTroopCount(map[playerFocusX][playerFocusY].getTroopCount() + playerControlledTroopCount);
+				if (map[pX][pY].getOwnedBy().compareTo(pID) == 0) {
+					map[pX][pY].setTroopCount(map[pX][pY].getTroopCount() + pTroops);
 				} 
 				else { // If we do not own the tile, we need to check if there are troops on it and subtract from our own
-					if (playerControlledTroopCount > map[playerFocusX][playerFocusY].getTroopCount()) {
-						playerControlledTroopCount -= map[playerFocusX][playerFocusY].getTroopCount();
-						map[playerFocusX][playerFocusY].setOwnedBy(playerID);
-						map[playerFocusX][playerFocusY].setBackgroundColor(playerColor);
-						map[playerFocusX][playerFocusY].setTroopCount(playerControlledTroopCount);
+					if (pTroops > map[pX][pY].getTroopCount()) {
+						pTroops -= map[pX][pY].getTroopCount();
+						map[pX][pY].updateTile(-1, pID, pColor, pTroops, null); 
 					} else { // If we do not have enough troops, our count is set to zero, we do not take over the tile
-						map[playerFocusX][playerFocusY].setTroopCount(map[playerFocusX][playerFocusY].getTroopCount() - playerControlledTroopCount);
-						playerControlledTroopCount = 0;
+						map[pX][pY].setTroopCount(map[pX][pY].getTroopCount() - pTroops);
+						pTroops = 0;
 					}
 				}
 			}
@@ -392,14 +393,14 @@ public class GamePanel extends JPanel implements ActionListener{
 	public class MyMouseAdapter extends MouseAdapter {
 		@Override
 		public void mouseClicked(MouseEvent e) {
-			playerFocusX = e.getX() / TILESIZE ;
-			playerFocusY = e.getY() / TILESIZE ;
+			pX = e.getX() / TILESIZE ;
+			pY = e.getY() / TILESIZE ;
 
 			// If we own the tile we clicked on, switch the controlled troops to the number of troops on that tile
-			if (map[playerFocusX][playerFocusY].getOwnedBy().compareTo(playerID) == 0) {
-				playerControlledTroopCount = map[playerFocusX][playerFocusY].getTroopCount();
+			if (map[pX][pY].getOwnedBy().compareTo(pID) == 0) {
+				pTroops = map[pX][pY].getTroopCount();
 			} else {
-				playerControlledTroopCount = 0;
+				pTroops = 0;
 			}
 		}
 	}
@@ -411,10 +412,10 @@ public class GamePanel extends JPanel implements ActionListener{
 	 */
 	public void debug() {
 		System.out.println(">>: Calling Update... :<<");
-		System.out.println("Current Tile owned by:		" + map[playerFocusX][playerFocusY].getOwnedBy());
-		System.out.println("Current Tile Troop Count:	" + map[playerFocusX][playerFocusY].getTroopCount());
-		System.out.println("Current playerControlledTroops: " + playerControlledTroopCount);
-		System.out.println("Current Tile Type:	" + map[playerFocusX][playerFocusY].getTileType());
+		System.out.println("Current Tile owned by:		" + map[pX][pY].getOwnedBy());
+		System.out.println("Current Tile Troop Count:	" + map[pX][pY].getTroopCount());
+		System.out.println("Current playerControlledTroops: " + pTroops);
+		System.out.println("Current Tile Type:	" + map[pX][pY].getTileType());
 		System.out.println();
 	}
 }
--- Generals/src/Tile.java
+++ Generals/src/Tile.java
@@ -4,37 +4,13 @@ import java.awt.Image;
 public class Tile {
 	private int x;
 	private int y;
-	private int tileType = 0; //[0 = Empty : 1 = Mountain : 6 = Capitol : 2 = City : 3 = Swamp]
+	private int tileType = 0; //[0 = Empty : 1 = Mountain : 2 = City : 3 = Swamp : 6 = Capitol]
 	private String ownedBy = "noOne";
 	private int troopCount = 0;
 	private Color backgroundColor = new Color(222, 222, 222);
 	private Image icon = null;	// Null by Default
 	
 	
-	public Image getIcon() {
-		return icon;
-	}
-
-	public void setIcon(Image icon) {
-		this.icon = icon;
-	}
-
-	public int getTroopCount() {
-		return troopCount;
-	}
-
-	public void setTroopCount(int troopCount) {
-		this.troopCount = troopCount;
-	}
-	
-	public Color getBackgroundColor() {
-		return backgroundColor;
-	}
-
-	public void setBackgroundColor(Color backgroundColor) {
-		this.backgroundColor = backgroundColor;
-	}
-
 	Tile(int x, int y){
 		this.x = x;
 		this.y = y;
@@ -46,6 +22,45 @@ public class Tile {
 		setOwnedBy(ownedBy);
 	}
 	
+	/**
+	 * 	This method will take in the tileType, new ownerID and new Color to update the current tile
+	 * 
+	 * @param type : pass in an int for the tile type [Default = 0 : Mountain = 1 : City = 2 : Swamp = 3 : Capitol = 6]
+	 * @param owner : the new string for the ID you want this tile to hold
+	 * @param backgroundColor : the new background color, most likely the player color
+	 * @param troops : this integer value will be set to the currentTroop count on the tile
+	 */
+	public void updateTile(int type, String ID, Color color, int troops, Image icon) {
+		if (type != -1) { this.tileType = type; }
+		this.ownedBy = ID;
+		this.backgroundColor = color;
+		this.troopCount = troops;
+		if (icon != null) { this.icon = icon; }
+	}
+	
+	
+//############################################################
+//		Nasty Nasty Getters and Settlers
+//############################################################
+	
+	public Image getIcon() {
+		return icon;
+	}
+	public void setIcon(Image icon) {
+		this.icon = icon;
+	}
+	public int getTroopCount() {
+		return troopCount;
+	}
+	public void setTroopCount(int troopCount) {
+		this.troopCount = troopCount;
+	}
+	public Color getBackgroundColor() {
+		return backgroundColor;
+	}
+	public void setBackgroundColor(Color backgroundColor) {
+		this.backgroundColor = backgroundColor;
+	}
 	public String getOwnedBy() {
 		return ownedBy;
 	}
@@ -70,6 +85,5 @@ public class Tile {
 	public void setTileType(int tileType) {
 		this.tileType = tileType;
 	}
-	
 }
 
